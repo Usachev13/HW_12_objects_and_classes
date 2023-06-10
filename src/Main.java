131b803e847ad796382cc62fbfc6457b8f0200f2
Usachev13 <2893318@gmail.com>
2023-06-10 13:41:34 +0300

    library done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author huxley = new Author("Олдос", "Хаксли");
+        Author orwell = new Author("Джордж", "Оруэлл");
+        Book braveNewWorld = new Book("Дивный новый мир", huxley, 1932);
+        Book oneNineEightFour = new Book("1984", orwell, 1949);
+        System.out.println(braveNewWorld.getName() + " - " + braveNewWorld.getAuthor().getName() + " " + braveNewWorld.getAuthor().getSurname() + " - " + braveNewWorld.getYear());
+        System.out.println(oneNineEightFour.getName() + " - " + oneNineEightFour.getAuthor().getName() + " " + oneNineEightFour.getAuthor().getSurname() + " - " + oneNineEightFour.getYear());
+
+        braveNewWorld.setYear(1935);
+        System.out.println(braveNewWorld.getYear());
     }
 }
